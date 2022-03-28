5dcf4465430646cb49d2c77917d203db3c86ab84
ZefanyaOkta <zefanyaoktaviana@gmail.com>
2022-03-28 17:03:07 +0700

    update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -9,34 +9,23 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons" />
 
-    <link rel="stylesheet" href={{ asset('css/header.css') }} type="text/css"/>
-    <script type="text/javascript" src= {{ asset('js/header.js') }}></script>
+    <link rel="stylesheet" href="css/product.css" />
+    {{-- <script type="text/javascript" src="js/header.js"></script> --}}
 </head>
 
 
-<body>
-    {{-- <button id="btnScrollTop" onclick="scrollToTop()">
+<body style="background-color:white;">
+
+    <button id="btnScrollTop" onclick="scrollToTop()">
         <i class="material-icons">keyboard_double_arrow_up</i>
     </button>
 
     <div class="sidemenu">
         <img class="logo" src="images/logo/logo.png" style="height: 50%" alt="Logo" />
+
+    </div>
     </div>
 
-    </div> --}}
-
-    <!-- Side navigation -->
-<div class="sidenav">
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="#">Contact</a>
-  </div>
-
-  <!-- Page content -->
-  <div class="main">
-    ...
-  </div>
 </body>
 
 </html>
