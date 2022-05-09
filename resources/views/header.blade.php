797a6dd2dd4452ed29c909640cf138140e732632
NathaliaMinoque <jr.minoqueen@gmail.com>
2022-05-09 15:48:16 +0700

    .

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -91,13 +91,13 @@
             <a style="padding-right: 18%; float: right; margin-top: 5px"
             class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuLink"
             role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-            <i class="fas fa-user" style="color: black">
+            <i class="fas fa-user" style="color: black; position: fixed;">
                 <p style="font-family: Poppins; margin-left: 5px; float: right;"> A001 </p>
             </i>
         </a>
-        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink" style="position: fixed;">
             <li>
-                <a style="text-align: left; color: black;" class="dropdown-item" href="/login">Log Out</a>
+                <a style="text-align: left; color: black; position: fixed; right: -150px;" class="dropdown-item" href="/login">Log Out</a>
             </li>
         </ul>
         </div>
