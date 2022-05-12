dc8032359805df24413405d5617055d77bc1d2a0
NathaliaMinoque <jr.minoqueen@gmail.com>
2022-05-12 13:26:42 +0700

    error mboh

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -112,6 +112,19 @@
         <hr class="topbar-hr">
     </div>
 
+     <!-- Chart's container -->
+     <div id="chart" style="height: 300px;"></div>
+     <!-- Charting library -->
+     <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
+     <!-- Chartisan -->
+     <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
+     <!-- Your application script -->
+     <script>
+       const chart = new Chartisan({
+         el: '#chart',
+         url: "@chart('sample_chart')",
+       });
+     </script>
 </body>
 
 </html>
