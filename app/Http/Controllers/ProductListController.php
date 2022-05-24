5e2ac56b0e088f800d9cea729cd16b48d8519bea
shalomeiravw <shalomeiravalencia@gmail.com>
2022-05-24 16:10:52 +0700

    .

--- app/Http/Controllers/ProductListController.php
+++ app/Http/Controllers/ProductListController.php
@@ -44,27 +44,21 @@ class ProductListController extends Controller
 
         // $product = DB::table('PRODUCT')
         //     ->join('DETAIL_TRANSACTION', 'PRODUCT.SKU', '=', 'DETAIL_TRANSACTION.SKU')
-        //     ->select('PRODUCT.*', SUM('DETAIL_TRANSACTION'.'QTY_PRODUCT'))
+        //     ->select('PRODUCT.*', DB::raw("SUM('DETAIL_TRANSACTION'.'QTY_PRODUCT')"))
         //     ->groupBy('SKU')
-        //     ->orderBy(SUM('DETAIL_TRANSACTION'.'QTY_PRODUCT'), 'desc')
+        //     ->orderBy(DB::raw("SUM('DETAIL_TRANSACTION'.'QTY_PRODUCT')"))
         //     ->get();
+
+        // $product = DB::table('PRODUCT')
+        //         ->select('PRODUCT.SKU', 'ID_CATEGORY', 'P_NAME', 'SIZE', 'PRICE', 'STOCK', 'DESCRIPTION', 'IMAGE', DB::raw('IFNULL'(SUM('DETAIL_TRANSACTION.QTY_PRODUCT'), 0))
+        //         ->leftJoin('DETAIL_TRANSACTION','PRODUCT.SKU','=','DETAIL_TRANSACTION.SKU')
+        //         ->where('PRODUCT.STATUS_DELETE','=',0)
+        //         ->groupBy('PRODUCT.SKU','ID_CATEGORY','P_NAME','SIZE','PRICE','STOCK','DESCRIPTION','IMAGE')
+        //         ->get();
 
         return view("home", [
             "product" => $product
                 ->take(5)
-            // "sales" => $sales
-            //     ->take(4)
         ]);
     }
-
-    // public function total_sales()
-    // {
-    //     $product = ProductListModel::all();
-
-    //     return view("home", [
-    //         "product" => $product
-
-    //             ->take(5)
-    //     ]);
-    // }
 }
