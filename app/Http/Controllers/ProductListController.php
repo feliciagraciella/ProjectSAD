49f156f637639e30cf446a9a07b986a0066a3893
ZefanyaOkta <zefanyaoktaviana@gmail.com>
2022-05-25 22:06:39 +0700

    .

--- app/Http/Controllers/ProductListController.php
+++ app/Http/Controllers/ProductListController.php
@@ -21,6 +21,69 @@ class ProductListController extends Controller
         ]);
     }
 
+    public function addproduct(Request $request)
+    {
+        $messages = array();
+        $sku = $_POST['sku'];
+        $name = $_POST['name'];
+        $cat = $_POST['cat'];
+        $price = $_POST['price'];
+        $qty = $_POST['qty'];
+        $size = $_POST['size'];
+        $desc = $_POST['desc'];
+        $image = $_POST['image'];
+
+        //validasi input
+        if($sku == ''){
+            array_push($messages, 'SKU must be filled out.');
+        }
+        if($name == ''){
+            array_push($messages, 'Name must be filled out.');
+        }
+        if($cat == ''){
+            array_push($messages, 'Category name must be filled out.');
+        }
+        if($price == ''){
+            array_push($messages, 'Price must be filled out.');
+        }
+        if($qty == ''){
+            array_push($messages, 'Qty must be filled out.');
+        }
+        if($size == ''){
+            array_push($messages, 'Size must be filled out.');
+        }
+        if($desc == ''){
+            array_push($messages, 'Description must be filled out.');
+        }
+        // if($image == ''){
+        //     array_push($messages, 'Image must be filled out.');
+        // }
+        if (isset($messages) && count($messages)>0){
+            return redirect('/product');
+        }
+
+        $data = [
+            'sku' => $sku,
+            'name' => $name,
+            'cat' => $cat,
+            'price' => $price,
+            'qty' => $qty,
+            'size' => $size,
+            'desc' => $desc
+        ];
+
+        $user = New ProductListModel();
+        $flag_exist = $user->addproduct($data);
+
+        if($flag_exist==1){
+            return redirect('/product');
+        }
+
+        //menyimpan foto produk
+        $data = $request->all();
+        $data['image'] = $request->file('image')->store('images', 'public');
+    }
+
     // public function productdetail($sku)
     // {
     //     $item = DB::table('PRODUCT')
--- app/Models/ProductListModel.php
+++ app/Models/ProductListModel.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductListModel extends Model
 {
+    public function addproduct($data)
+    {
+        $cmd = "INSERT INTO(sku, id_category, p_name, size, price, stock, `description`, `image`)
+        VALUES (:sku, 'A', :name, :size, :price, :qty, :desc, '')";
+        $res = DB::insert($cmd, $data);
+        return $res;
+    }
     use HasFactory;
 
     protected $table = "PRODUCT";
