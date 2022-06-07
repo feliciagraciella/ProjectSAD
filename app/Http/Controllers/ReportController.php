17492cc3910c35b795367571c117ae2a47a5fa51
NathaliaMinoque <jr.minoqueen@gmail.com>
2022-06-08 00:58:29 +0700

    chart

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,27 +12,28 @@ use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
-    //
-    // function __construct()
-    // {
-    //     $this->LihatDataModel = new LihatDataModel();
-    // }
-
-
-
     public function report()
     {
         $data = DB::select("select * from product_all_all");
-
+        $totalsold = DB::table('product_all_all')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
         $income = DB::select("select fNetProfit() as `NetProfit`");
-
         $admin = DB::select("select fAdminFee() as `AdminFee`");
-        // $dreport = "select * from most_popular_all ";
+
+        foreach($data as $d){
+            $namaproduk[] = (string)$d->PRODUCT_NAME;
+            $jumlahproduk[] = (int)$d->TOTAL_SOLD;
+        }
+
+        // dd($namaproduk,$jumlahproduk);
         return view("report", [
             "data" => $data,
             "income" => $income[0]->NetProfit,
             "admin" => $admin[0]->AdminFee,
-            "reportname" => "Product Sales/All/All"
+            "reportname" => "Product Sales Report",
+            "reportsub" => "All Period on All Platform",
+            "namaproduk" => $namaproduk,
+            "jumlahproduk" => $jumlahproduk,
+            "totalsold" => $totalsold[0]->SUM
         ]);
 
     }
@@ -42,19 +43,20 @@ class ReportController extends Controller
         $report = $req->get("select_report");
         $platform = $req->get("select_platform");
         $period = $req->get("select_period");
-        $reportname = $report."/".$platform."/".$period;
-        // dd($reportname);
+        $reportname = $report." Report";
+        $reportsub = $period." Period on ".$platform." Platform";
+        // dd($reportsub);
         if($report == "Product Sales"){
             if($platform == "All" && $period == "All"){
                 $data = DB::select("select * from product_all_all");
-
+                $totalsold = DB::table('product_all_all')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "All" && $period == "Last 7 Days"){
                 $data = DB::select("select * from product_all_seven");
-
+                $totalsold = DB::table('product_all_seven')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
@@ -62,59 +64,69 @@ class ReportController extends Controller
             }
             elseif($platform == "All" && $period == "Last 30 Days"){
                 $data = DB::select("select * from product_all_thirty");
-
+                $totalsold = DB::table('product_all_thirty')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "Shopee" && $period == "All"){
                 $data = DB::select("select * from product_shopee_all");
-
+                $totalsold = DB::table('product_shopee_all')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "Shopee" && $period == "Last 7 Days"){
                 $data = DB::select("select * from product_shopee_seven");
-
+                $totalsold = DB::table('product_shopee_seven')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "Shopee" && $period == "Last 30 Days"){
                 $data = DB::select("select * from product_shopee_thirty");
-
+                $totalsold = DB::table('product_shopee_thirty')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "Tokopedia" && $period == "All"){
                 $data = DB::select("select * from product_tokopedia_all");
-
+                $totalsold = DB::table('product_tokopedia_all')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "Tokopedia" && $period == "Last 7 Days"){
                 $data = DB::select("select * from product_tokopedia_seven");
-
+                $totalsold = DB::table('product_tokopedia_seven')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             elseif($platform == "Tokopedia" && $period == "Last 30 Days"){
                 $data = DB::select("select * from product_tokopedia_thirty");
-
+                $totalsold = DB::table('product_tokopedia_thirty')->select(DB::raw('SUM(TOTAL_SOLD) as `SUM`'))->get();
                 $income = DB::select("select fNetProfit() as `NetProfit`");
 
                 $admin = DB::select("select fAdminFee() as `AdminFee`");
             }
             // dd($report);
+
+            foreach($data as $d){
+                $namaproduk[] = $d->PRODUCT_NAME;
+                $jumlahproduk[] = (int)$d->TOTAL_SOLD;
+            }
+
             return view("report", [
                 "data" => $data,
                 "income" => $income[0]->NetProfit,
                 "admin" => $admin[0]->AdminFee,
-                "reportname" => $reportname
+                "reportname" => $reportname,
+                "reportsub" => $reportsub,
+                "namaproduk" => $namaproduk,
+                "jumlahproduk" => $jumlahproduk,
+                "totalsold" => $totalsold[0]->SUM
             ]);
         }
 
@@ -188,7 +200,8 @@ class ReportController extends Controller
                 "data" => $data,
                 "income" => $income[0]->NetProfit,
                 "admin" => $admin[0]->AdminFee,
-                "reportname" => $reportname
+                "reportname" => $reportname,
+                "reportsub" => $reportsub
             ]);
         }
 
