eff556846cfe9837e6dbe6d401f9881856a7ff3d
shalomeiravw <shalomeiravalencia@gmail.com>
2022-06-10 15:19:41 +0700

    .

--- app/Http/Controllers/LogInController.php
+++ app/Http/Controllers/LogInController.php
@@ -62,7 +62,7 @@ class LogInController extends Controller
         $idadmin = $request->input('admin');
         $password = $request->input('password');
         $userdata = DB::table('ADMIN')->where('ID_ADMIN', $idadmin)->first();
-        dd($userdata);
+        // dd($userdata);
         // $obj = get_object_vars($userdata);
         // $request->session()->put('idadmin', $obj['ID_ADMIN']);
         if (is_null($userdata)) {
